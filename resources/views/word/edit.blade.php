c0ece2cfc259469b2c70ffa97fa991fa19ae07e1
yuanhy585 <1269971258@qq.com>
2016-10-29 21:01:05 +0800

    some changes have been made in the edit view

--- resources/views/word/edit.blade.php
+++ resources/views/word/edit.blade.php
@@ -15,14 +15,14 @@
         </div>
 
         <div class="col-md-10 col-md-offset-1" style="text-align: left;">
-            <h4>Spelling:</h4>
+            <h4>pronunciation:</h4>
             <input type="text" class="form-control" name="pronunciation"
                    value="{{$word->pronunciation}}"/>
             <br/><br/>
         </div>
 
         <div class="col-md-10 col-md-offset-1" style="text-align: left;">
-            <h4>Spelling:</h4>
+            <h4>meaning:</h4>
             <input type="text" class="form-control" name="meaning"
                    value="{{$word->meaning}}"/>
             <br/><br/>
@@ -30,7 +30,7 @@
 
         <div class="col-md-10 col-md-offset-1" style="text-align: left;">
             <button type="submit" class="btn btn-primary">确认修改</button>
-            <button type="button" class="btn btn-danger">取消修改</button>
+            <button type="submit" class="btn btn-danger">取消修改</button>
         </div>
 
     </form>
