9b12b7044e1a97f040920e74733f8100a2db27ba
yuanhy585 <1269971258@qq.com>
2017-01-28 11:49:00 +0800

    subtle revisions in qrcdoe view

--- resources/views/word/QrCode.blade.php
+++ resources/views/word/QrCode.blade.php
@@ -2,9 +2,12 @@
 
 @section('content')
     <div class="visible-print text-center">
-        {!! QrCode::size(100)->generate('https://www.baidu.com'); !!}
+{{--        {!! QrCode::size(100)->generate('https://www.baidu.com'); !!}--}}
+        {!! QrCode::size(100)->encoding('utf-8')->color(255,0,255)->backgroundColor(255,255,0)
+        ->generate('你好，Laravel学院！'); !!}
+
         <p>
-            Scan me to return to hte original page.
+            Scan me to return to the original page.
         </p>
     </div>
 @stop
